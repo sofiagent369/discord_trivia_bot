46f2c1e5a1b383924b7974b57414afe5a86f7320
Sofia <sofiagent369@gmail.com>
2026-06-24 10:20:26 +0200

    [Crítico] Aplicadas correcciones tras revisión

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import asyncio  # Added this import
 from db import SessionLocal, Trivia, Question
 
 # Cargar variables de entorno desde .env
